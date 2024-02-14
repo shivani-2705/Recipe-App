4c12274ab9382da87a68d0146c4052ff2c4103c6
shivani-2705 <131511599+shivani-2705@users.noreply.github.com>
2024-02-14 18:44:09 +0530

    Add files via upload

---
+++ server/src/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+import {userRouter} from "../routes/users.js"
+import {recipeRouter} from "../routes/recipes.js"
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth" , userRouter);
+app.use("/recipes", recipeRouter);
+
+mongoose.connect(
+       "mongodb+srv://shivanisingh:kimtaehyung@recipes.k8wk9sy.mongodb.net/recipes?retryWrites=true&w=majority"
+       )
+
+
+ app.listen(3001 , () => {
+        console.log("Server is running");
+ });
+
+
